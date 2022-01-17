b92f4f8e601accd7e85b76d948679bba7a4b6e8e
DESKTOP-0OVTI4S\toliv <tolive@live.ru>
2022-01-17 16:27:38 +0300

    Добавьте файлы проекта.

---
+++ Lesson_01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("See attach pic");
+            Console.ReadKey();
+        }
+    }
+}
